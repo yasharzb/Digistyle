eff901c9f29e7cf7ccc6bcf526fa0899f9ff7b95
yasharzb <yasharzb@ce.sharif.edu>
2020-11-20 19:11:59 +0330

    Finalizing V1__digi_style.sql

--- V1__digi_style.sql
+++ V1__digi_style.sql
@@ -12,8 +12,6 @@ CREATE TABLE user
     PRIMARY KEY (email)
 );
 
-
-# TODO add foreign key for seller
 CREATE TABLE clothing
 (
     id       varchar(255) NOT NULL PRIMARY KEY,
@@ -25,11 +23,12 @@ CREATE TABLE clothing
 
 CREATE TABLE variety
 (
-    id          varchar(255) NOT NULL PRIMARY KEY,
+    id          varchar(255) NOT NULL,
     color       varchar(255) NOT NULL,
     size        varchar(255) NOT NULL,
     count       integer      NOT NULL,
     clothing_id varchar(255) NOT NULL,
+    PRIMARY KEY (id, clothing_id),
     FOREIGN KEY (clothing_id) REFERENCES clothing (id)
 );
 
@@ -39,23 +38,66 @@ CREATE TABLE comments
     score       integer      NOT NULL,
     text        TEXT DEFAULT NULL,
     clothing_id varchar(255) NOT NULL,
-    user_email  varchar(255) NOT NULL,
-    PRIMARY KEY (id, user_email),
-    FOREIGN KEY (user_email) REFERENCES user (email),
+    user_id  varchar(255) NOT NULL,
+    PRIMARY KEY (id, user_id),
+    FOREIGN KEY (user_id) REFERENCES user (email),
     FOREIGN KEY (clothing_id) REFERENCES clothing (id)
 );
 
-
-
 CREATE TABLE in_cart
 (
-    user_email varchar(255) NOT NULL,
+    user_id varchar(255) NOT NULL,
     variety_id varchar(255) NOT NULL,
     count      integer      NOT NULL,
-    PRIMARY KEY (user_email, variety_id),
-    FOREIGN KEY (user_email) REFERENCES user (email)
+    PRIMARY KEY (user_id, variety_id),
+    FOREIGN KEY (user_id) REFERENCES user (email)
 );
 
+CREATE TABLE driver
+(
+    name      varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    plate     varchar(8)   NOT NULL,
+    phone_no  varchar(12)  NOT NULL,
+    id        varchar(10)  NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE schedule
+(
+    id            varchar(255) NOT NULL,
+    delivery_date date,
+    capacity      int,
+    driver_id     varchar(10)  NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (driver_id) REFERENCES driver (id)
+);
+
+CREATE TABLE product_order
+(
+    id              bigint       NOT NULL,
+    order_date      date         NOT NULL,
+    delivery_method int          NOT NULL,
+    state           int          NOT NULL,
+    schedule_id     varchar(255) NOT NULL,
+    user_id         varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (schedule_id) REFERENCES schedule (id),
+    FOREIGN KEY (user_id) REFERENCES user (email)
+);
+
+CREATE TABLE product_order_variety
+(
+    order_id    bigint NOT NULL,
+    variety_id  varchar(255),
+    clothing_id varchar(255),
+    FOREIGN KEY (order_id) REFERENCES product_order (id),
+    FOREIGN KEY (variety_id, clothing_id) REFERENCES variety (id, clothing_id)
+);
+
+
+INSERT INTO user
+VALUES ('mmd', 'mmd@mmd.mmd', 'mmd_abad', NULL, '123456789012', 'mmd');
 
 INSERT INTO clothing
 VALUES ('shk1001', 'shalvar_kordi', 'todo_add_seller', 'shalvar_rahat', 1000);
@@ -63,11 +105,20 @@ VALUES ('shk1001', 'shalvar_kordi', 'todo_add_seller', 'shalvar_rahat', 1000);
 INSERT INTO variety
 VALUES ('sub_cloth_id', 'brown', 'XL', 1000000, 'shk1001');
 
-INSERT INTO user
-VALUES ('mmd', 'mmd@mmd.mmd', 'mmd_abad', NULL, '123456789012', 'mmd');
-
 INSERT INTO in_cart
 VALUES ('mmd@mmd.mmd', 'sub_cloth_id', 100);
 
 INSERT INTO comments
 VALUES ('first_comment_id', 5, 'toosh gom mishe adadm', 'shk1001', 'mmd@mmd.mmd');
+
+INSERT INTO driver
+VALUES ('hasan', 'kachal', '82b34522', '9123456789', '0123456789');
+
+INSERT INTO schedule
+VALUES ('tx123', '2020-11-20', 5, '0123456789');
+
+INSERT INTO product_order
+VALUES (385928, '2020-11-15', '2', '1', 'tx123', 'mmd@mmd.mmd');
+
+INSERT INTO product_order_variety
+VALUES (385928, 'sub_cloth_id', 'shk1001');
